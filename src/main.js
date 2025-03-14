60a56cd7ab37683e4dfc341b4163b943cdf0c9a8
Augusto de Moraes Piatto <ampiatto@hotmail.com>
2025-03-14 20:14:35 -0300

    build(ant-design): instala dependencia e faz funcionar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import { createApp } from 'vue';
 import '@/styles/main.scss';
-import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from '@/router/router';
+import Antd from 'ant-design-vue';
+import { createApp } from 'vue';
 import { makeServer } from '../mirage.config';
+import { createPinia } from 'pinia';
+import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App);
 
+app.use(Antd);
 app.use(router);
 app.use(createPinia());
 
