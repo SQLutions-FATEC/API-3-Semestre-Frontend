280997c74ef4b04367ea03b2eeadb20b6d1be8cd
BryanARM <bryanassis244@gmail.com>
2025-03-21 21:27:56 -0300

    feat(employee): Adiciona textfields a pagina de funcionario

--- src/pages/Employee/Employee.vue
+++ src/pages/Employee/Employee.vue
@@ -22,5 +22,32 @@ export default {
 </script>
 
 <template>
-  <a-button type="primary" @click="count++">{{ count }}</a-button>
+  <div class="employee">
+    <h1>Cadastro de empresa</h1>
+    <div class="employee__content">
+      <div class="content__input">
+        <a-input v-model:value="employeeName" placeholder="Nome completo" />
+      </div>
+      <div class="content__input">
+        <a-input v-model:value="cpf" placeholder="CPF" />
+      </div>
+      <div class="ant-dropdown-link">
+        <a-input v-model:value="birthDate" placeholder="Data de nascimento" />
+      </div>
+      <div class="content__input">
+        <a-input v-model:value="bloodType" placeholder="Tipo sanguíneo" />
+      </div>
+      <div class="content__input">
+        <a-input v-model:value="occupation" placeholder="Função" />
+      </div>
+      <div class="content__input">
+        <a-input v-model:value="company" placeholder="Empresa" />
+      </div>
+      <div class="content__action">
+        <a-button type="primary" style="width: 250px" @click="createEmployee">
+          Cadastrar
+        </a-button>
+      </div>
+    </div>
+  </div>
 </template>
