f7fc0dc3b29decc1843b53a5518fb507ba696c8c
Augusto de Moraes Piatto <ampiatto@hotmail.com>
2025-03-21 21:53:21 -0300

    feat(mock): faz o post de empresa

--- src/mock/routes/companyRoutes.js
+++ src/mock/routes/companyRoutes.js
@@ -1,13 +1,24 @@
 import { APIFailureWrapper, mockFlag } from '@/mock/utils.js';
+import { companies } from '@/mock/seeds/companySeeds';
 
 const companyRoutes = [
   mockFlag(
     {
       method: 'post',
       url: '/company',
-      result: () => {
+      result: ({ requestBody }) => {
+        const body = JSON.parse(requestBody);
+
+        const newCompany = {
+          id: companies.length + 1,
+          company_name: body.company_name,
+          cnpj: body.cnpj,
+          trade_name: body.trade_name,
+        };
+        companies.push(newCompany);
+
         return APIFailureWrapper({
-          content: {},
+          content: newCompany,
           errorMessage: 'Erro ao criar empresa',
         });
       },
