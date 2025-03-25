4dbb1449781a7ca99873bc677212dd9411ef2258
BryanARM <bryanassis244@gmail.com>
2025-03-24 22:25:53 -0300

    feat(employee): Adiciona scss, funções do dropdown e variaveis
    Adiciona o style do arquivo, funções que mudam o valor do dropdown e variaveis do empregado

--- src/pages/Employee/Employee.vue
+++ src/pages/Employee/Employee.vue
@@ -1,83 +1,151 @@
 <script>
-import { Button,  } from 'ant-design-vue';
-import { DownOutlined } from '@ant-design/icons-vue';
+
+import { Button, Dropdown, Input, Menu } from 'ant-design-vue';
+import { ref } from 'vue';
 
 export default {
   name: 'Employee',
 
   components: {
     'a-button': Button,
+    'a-input': Input,
+    'a-dropdown': Dropdown,
+    'a-menu': Menu,
+    'a-menu-item': Menu.Item,
   },
 
   setup() {
-    createEmployee
-    
-const handleMenuClick = ({ key }) => {
-  switch (key) {
-    case '1':
-      dropdownText.value = 'Opção 1 selecionada';
-      break;
-    case '2':
-      dropdownText.value = 'Opção 2 selecionada';
-      break;
-    case '3':
-      dropdownText.value = 'Opção 3 selecionada';
-      break;
-    default:
-      dropdownText.value = 'Selecione uma opção';
-};
-}
-  return {
-      createEmployee
+
+    const createEmployee = () => { return 0 };
+    const employeeName = ref('');
+    const employeeCpf = ref('');
+    const employeeBirthDate = ref('');
+    const employeeBloodType = ref('');
+    const employeeFunction = ref('Selecione uma função');
+    const company = ref('');
+
+
+    const handleMenuClick = ({ key }) => {
+      switch (key) {
+        case '1':
+          employeeFunction.value = 'Engenheiro';
+          break;
+        case '2':
+          employeeFunction.value = 'Mecânico';
+          break;
+        case '3':
+          employeeFunction.value = 'Pintor';
+          break;
+        default:
+          createEmployee
+          employeeFunction.value = 'Criar nova função';
+      }
+    };
+
+    return {
+      employeeName,
+      employeeCpf,
+      employeeBirthDate,
+      employeeBloodType,
+      employeeFunction,
+      company,
+      handleMenuClick,
+      createEmployee,
     };
   },
 };
+
 </script>
 <template>
   <div class="employee">
-    <h1>Cadastro de funcionario</h1>
-    <div class="employee__content">
+    <h1>Cadastro de Funcionário</h1>
+
+    <div class="employee_content">
       <div class="content__input">
         <a-input v-model:value="employeeName" placeholder="Nome completo" />
       </div>
+
       <div class="content__input">
-        <a-input v-model:value="cpf" placeholder="CPF" />
-      </div>
-      <div class="content__input">
-        <a-input v-model:value="birthDate" placeholder="Data de nascimento" />
-      </div>
-      <div class="content__input">
-        <a-input v-model:value="bloodType" placeholder="Tipo sanguíneo" />
+        <a-input v-model:value="employeeCpf" placeholder="CPF" />
       </div>
 
-      <a-dropdown>
-    <a class="ant-dropdown-link" @click.prevent>
-      Função
-      <DownOutlined />
-    </a>
-    <template #overlay>
-      <a-menu>
-        <a-menu-item key="1">
-          <a href="javascript:;">Engenheiro</a>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <a href="javascript:;">Pintor</a>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <a href="javascript:;">Criar nova função</a>
-        </a-menu-item>
-      </a-menu>
-    </template>
-  </a-dropdown>
+      <div class="content__input">
+        <a-input v-model:value="employeeBirthDate" placeholder="Data de nascimento" />
+      </div>
+
+      <div class="content__input">
+        <a-input v-model:value="employeeBloodType" placeholder="Tipo sanguíneo" />
+      </div>
+
+      <div class="dropdown">
+        <a-dropdown>
+
+          <a-button>
+            {{ employeeFunction }}
+          </a-button>
+
+          <template #overlay>
+            <a-menu @click="handleMenuClick">
+              <a-menu-item key="1">Engenheiro</a-menu-item>
+              <a-menu-item key="2">Mecânico</a-menu-item>
+              <a-menu-item key="3">Pintor</a-menu-item>
+              <a-menu-item key="4">Criar nova função</a-menu-item>
+            </a-menu>
+          </template>
+        </a-dropdown>
+      </div>
+
       <div class="content__input">
         <a-input v-model:value="company" placeholder="Empresa" />
       </div>
+
       <div class="content__action">
         <a-button type="primary" style="width: 250px" @click="createEmployee">
           Cadastrar
         </a-button>
       </div>
+
     </div>
   </div>
 </template>
 
+<style lang="scss" scoped>
+.employee {
+  padding: $spacingXxl;
+
+  .employee_content {
+    padding: $spacingXxl 0px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 32px;
+
+    .content__input {
+      flex: 0 0 calc(50% - 128px);
+      box-sizing: border-box;
+    }
+
+    .content__action {
+      flex: 0 0 calc(100% - 256px);
+      display: inline-flex;
+      justify-content: center;
+    }
+
+    .dropdown {
+      flex: 0 0 calc(50% - 128px);
+      box-sizing: border-box;
+
+      :deep(.ant-dropdown-trigger) {
+
+        text-align: left;
+        width: 100%;
+
+        button {
+          width: 100%;
+          text-align: left;
+        }
+      }
+    }
+
+  }
+}
+</style>
