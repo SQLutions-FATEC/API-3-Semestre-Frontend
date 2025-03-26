fec6008392135db4235510d04ea78638b806736d
EnzoLFranco <fenzolemos@gmail.com>
2025-03-26 14:23:34 -0300

    refactor(employee-edit): Substitui dropdowns por cascaders e simplifica a lógica de seleção

--- src/pages/Employee/EmployeeEdit.vue
+++ src/pages/Employee/EmployeeEdit.vue
@@ -1,19 +1,14 @@
 <script>
-
-import { Button, Dropdown, Input, Menu } from 'ant-design-vue';
+import { Button, Cascader, Input } from 'ant-design-vue';
 import { ref } from 'vue';
 
 export default {
   name: 'EmployeeEdit',
-
   components: {
     'a-button': Button,
     'a-input': Input,
-    'a-dropdown': Dropdown,
-    'a-menu': Menu,
-    'a-menu-item': Menu.Item,
+    'a-cascader': Cascader
   },
-
   setup() {
     const employeeName = ref('');
     const employeeCpf = ref('');
@@ -24,84 +19,63 @@ export default {
     const isEditing = ref(false);
     const profileImage = ref('https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg');
 
-    const editEmployee = () => {return 0};
-
     const toggleEdit = () => {
-        isEditing.value = !isEditing.value;
+      isEditing.value = !isEditing.value;
     };
 
-    const handleMenuClickBloodType = ({ key }) => {
-      switch (key) {
-        case '1': employeeBloodType.value = 'A+'; 
-        break;
-        case '2': employeeBloodType.value = 'A-'; 
-        break;
-        case '3': employeeBloodType.value = 'B+'; 
-        break;
-        case '4': employeeBloodType.value = 'B-'; 
-        break;
-        case '5': employeeBloodType.value = 'AB+'; 
-        break;
-        case '6': employeeBloodType.value = 'AB-'; 
-        break;
-        case '7': employeeBloodType.value = 'O+'; 
-        break;
-        case '8': employeeBloodType.value = 'O-'; 
-        break;
-      }
-    };
+    const bloodTypeOptions = [
+      { value: 'A+', label: 'A+' },
+      { value: 'A-', label: 'A-' },
+      { value: 'B+', label: 'B+' },
+      { value: 'B-', label: 'B-' },
+      { value: 'AB+', label: 'AB+' },
+      { value: 'AB-', label: 'AB-' },
+      { value: 'O+', label: 'O+' },
+      { value: 'O-', label: 'O-' }
+    ];
 
-    const handleMenuClickCompany = ({ key }) => {
-      switch (key) {
-        case '1': company.value = 'Empresa A'; 
-        break;
-        case '2': company.value = 'Empresa B'; 
-        break;
-        case '3': company.value = 'Empresa C'; 
-        break;
-        default: company.value = 'Adicionar uma empresa';
-      }
-    };
+    const functionOptions = [
+      { value: 'Engenheiro', label: 'Engenheiro' },
+      { value: 'Mecânico', label: 'Mecânico' },
+      { value: 'Pintor', label: 'Pintor' },
+      { value: 'Criar nova função', label: 'Criar nova função' }
+    ];
 
-    const handleMenuClickFunction = ({ key }) => {
-      switch (key) {
-        case '1': employeeFunction.value = 'Engenheiro'; 
-        break;
-        case '2': employeeFunction.value = 'Mecânico'; 
-        break;
-        case '3': employeeFunction.value = 'Pintor'; 
-        break;
-        default: employeeFunction.value = 'Criar nova função';
-      }
-    };
+    const companyOptions = [
+      { value: 'Empresa A', label: 'Empresa A' },
+      { value: 'Empresa B', label: 'Empresa B' },
+      { value: 'Empresa C', label: 'Empresa C' },
+      { value: 'Adicionar uma empresa', label: 'Adicionar uma empresa' }
+    ];
 
-    return{
-        toggleEdit,
-        editEmployee,
-        employeeName,
-        employeeCpf,
-        employeeBirthDate,
-        employeeBloodType,
-        employeeFunction,
-        company,
-        isEditing,
-        handleMenuClickFunction,
-        handleMenuClickCompany,
-        handleMenuClickBloodType,
-        profileImage,
+    return {
+      toggleEdit,
+      employeeName,
+      employeeCpf,
+      employeeBirthDate,
+      employeeBloodType,
+      employeeFunction,
+      company,
+      isEditing,
+      profileImage,
+      bloodTypeOptions,
+      functionOptions,
+      companyOptions,
     };
-    },
-}; 
+  }
+};
 </script>
+
 <template>
- <div class="edit_employee">
+  <div class="edit_employee">
     <h1>Edição de Funcionário</h1>
     <div class="edit_employee_content">
       <div class="profile-picture">
         <img :src="profileImage" alt="Profile Picture" />
       </div>
-       <div class="content__input">
-            <a-input v-model:value="employeeName" placeholder="Nome completo" :disabled="!isEditing" />
+
+      <div class="content__input">
+        <a-input v-model:value="employeeName" placeholder="Nome completo" :disabled="!isEditing" />
       </div>
 
       <div class="content__input">
@@ -111,57 +85,32 @@ export default {
       <div class="content__input">
         <a-date-picker v-model:value="employeeBirthDate" placeholder="Data de nascimento" :disabled="!isEditing" />
       </div>
-      
-      <div class="dropdown">
-        <a-dropdown>
-          <a-button :disabled="!isEditing">
-            {{ employeeBloodType }}
-          </a-button>
-          <template #overlay>
-            <a-menu @click="handleMenuClickBloodType">
-              <a-menu-item key="1">A+</a-menu-item>
-              <a-menu-item key="2">A-</a-menu-item>
-              <a-menu-item key="3">B+</a-menu-item>
-              <a-menu-item key="4">B-</a-menu-item>
-              <a-menu-item key="5">AB+</a-menu-item>
-              <a-menu-item key="6">AB-</a-menu-item>
-              <a-menu-item key="7">O+</a-menu-item>
-              <a-menu-item key="8">O-</a-menu-item>
-            </a-menu>
-          </template>
-        </a-dropdown>
+
+      <div class="content__input">
+        <a-cascader
+          :options="bloodTypeOptions"
+          placeholder="Tipo Sanguíneo"
+          :disabled="!isEditing"
+          @change="handleBloodTypeChange"
+        />
       </div>
 
-      <div class="dropdown">
-        <a-dropdown>
-          <a-button :disabled="!isEditing">
-            {{ employeeFunction }}
-          </a-button>
-          <template #overlay>
-            <a-menu @click="handleMenuClickFunction">
-              <a-menu-item key="1">Engenheiro</a-menu-item>
-              <a-menu-item key="2">Mecânico</a-menu-item>
-              <a-menu-item key="3">Pintor</a-menu-item>
-              <a-menu-item key="4">Criar nova função</a-menu-item>
-            </a-menu>
-          </template>
-        </a-dropdown>
+      <div class="content__input">
+        <a-cascader
+          :options="functionOptions"
+          placeholder="Função"
+          :disabled="!isEditing"
+          @change="handleFunctionChange"
+        />
       </div>
 
-      <div class="dropdown">
-        <a-dropdown>
-          <a-button :disabled="!isEditing">
-            {{ company }}
-          </a-button>
-          <template #overlay>
-            <a-menu @click="handleMenuClickCompany">
-              <a-menu-item key="1">Empresa A</a-menu-item>
-              <a-menu-item key="2">Empresa B</a-menu-item>
-              <a-menu-item key="3">Empresa C</a-menu-item>
-              <a-menu-item key="4">Adicionar uma empresa</a-menu-item>
-            </a-menu>
-          </template>
-        </a-dropdown>
+      <div class="content__input">
+        <a-cascader
+          :options="companyOptions"
+          placeholder="Empresa"
+          :disabled="!isEditing"
+          @change="handleCompanyChange"
+        />
       </div>
 
       <div class="content__action">
@@ -169,12 +118,12 @@ export default {
           {{ isEditing ? 'Confirmar' : 'Editar' }}
         </a-button>
       </div>
-
     </div>
- </div>
+  </div>
 </template>
-<style lang = "scss" scoped>
- .edit_employee {
+
+<style lang="scss" scoped>
+.edit_employee {
   padding: $spacingXxl;
 
   .edit_employee_content {
@@ -186,7 +135,6 @@ export default {
     .content__input {
       flex: 0 0 calc(50% - 16px);
       box-sizing: border-box;
-
     }
     .content__action {
       flex: 0 0 100%;
@@ -200,12 +148,12 @@ export default {
       margin-bottom: 16px;
 
       img {
-        width: 150px; 
-        height: 150px; 
-        border-radius: 50%; 
-        object-fit: cover; 
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+        object-fit: cover;
       }
-   }
+    }
   }
 }
 </style>
