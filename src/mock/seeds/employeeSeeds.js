16c7052bae842d864bce7c5bf9b56440788aa88b
BryanARMatheus <bryanassis244@gmail.com>
2025-03-28 23:50:14 -0300

    feat(mock): Adiciona dados falsos de empregado

---
+++ src/mock/seeds/employeeSeeds.js
@@ -0,0 +1,31 @@
+const employees = [
+  {
+    id: 1,
+    employee_name: "José Amaral",
+    employee_blood_type: "A+",
+    employee_function: "Engenheiro",
+    company: "Empresa",
+    employee_cpf: "543.765.234-55",
+    employee_birth_date: "02/02/1993",
+  },
+  {
+    id: 2,
+    employee_name: "Robson Fernandes",
+    employee_blood_type: "O+",
+    employee_function: "Soldador",
+    company: "Empresona",
+    employee_cpf: "986.321.654-32",
+    employee_birth_date: "26/08/1974",
+  },
+  {
+    id: 3,
+    employee_name: "Welington",
+    employee_blood_type: "B-",
+    employee_function: "Desenvolvedor",
+    company: "Empresinha",
+    employee_cpf: "765.423.978-33",
+    employee_birth_date: "12/11/1998",
+  },
+];
+
+export { employees };
