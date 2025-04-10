2213c9cb44988e9e3022d5ea22468a8a3a2ec589
BryanARMatheus <bryanassis244@gmail.com>
2025-04-09 21:00:02 -0300

    feat(46): Adds get and get by employee id methods on mock

--- src/services/employee.js
+++ src/services/employee.js
@@ -1,13 +1,15 @@
 import api from './api';
 
 const employee = {
+  get: () => api.get('/employee'),
+  get: (employeeId) => api.get(`/employee/${employeeId}`),
   create: (payload) =>
     api.post('/employee', {
       employee_name: payload.employee_name,
       employee_birth_date: payload.employee_birth_date,
       employee_blood_type: payload.employee_blood_type,
       employee_role: payload.employee_role,
-      company: payload.company,
+      company_id: payload.company_id,
       employee_rn: payload.employee_rn,
     }),
 };
