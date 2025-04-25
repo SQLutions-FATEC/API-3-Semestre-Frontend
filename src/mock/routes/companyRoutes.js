9c3369eabaac5032dcf92a9ff20c55ac1e4651d1
Enzo Lemos Franco <fenzolemos@gmail.com>
2025-04-24 21:34:00 -0300

    refactor(45): Remove duplicate imports

--- src/mock/routes/companyRoutes.js
+++ src/mock/routes/companyRoutes.js
@@ -1,8 +1,6 @@
 import { getClockInOut } from '@/mock/seeds/clockInOutSeeds';
 import { companies } from '@/mock/seeds/companySeeds';
 import { APIFailureWrapper, mockFlag } from '@/mock/utils.js';
-import { companies } from '@/mock/seeds/companySeeds';
-import { getClockInOut } from '@/mock/seeds/clockInOutSeeds';
 import {
   deleteClockInOut,
   updateCompanyInClockInOut,
