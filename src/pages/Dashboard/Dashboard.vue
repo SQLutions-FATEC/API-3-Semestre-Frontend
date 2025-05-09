67bd01fbeaf827e14955af8dfc907ecd281d8ee1
Augusto de Moraes Piatto <ampiatto@hotmail.com>
2025-05-09 20:03:39 -0300

    feat(91): create the begginning of dashboard layout
    pausing work

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,8 +1,24 @@
 <script>
+import { onMounted } from 'vue';
+
 export default {
   name: 'Dashboard',
 
   setup() {
+    const fetchDailyRegisters = () => {};
+
+    const fetchEmployeesByGender = () => {};
+
+    const fetchHoursWorkedByRole = () => {};
+
+    onMounted(() => {
+      const [dailyRegisters, hoursWorkedByRole, employeesByGender] =
+        Promise.all(
+          fetchDailyRegisters(),
+          fetchHoursWorkedByRole(),
+          fetchEmployeesByGender()
+        );
+    });
     return {};
   },
 };
@@ -10,12 +26,34 @@ export default {
 
 <template>
   <div class="dashboard">
-    <span>Oi</span>
+    <h1>Dashboard</h1>
+    <div class="dashboard__content">
+      <div class="daily-registers">
+        <span>{{ clockIn }}</span>
+        <span>{{ clockOut }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .dashboard {
   padding: $spacingXxl 0px;
+
+  h1 {
+    @include heading(large);
+  }
+  .dashboard__content {
+    padding: $spacingXxl 0px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: $spacingXxl;
+
+    .daily-registers {
+      padding: $spacingMd $spacingXl;
+      background-color: $colorBackgroundNeutral;
+      border-radius: $borderRadiusSm;
+    }
+  }
 }
 </style>
