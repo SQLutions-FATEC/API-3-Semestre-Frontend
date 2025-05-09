c0c5d6ccfb8209cfea7ba6f58656b80499eca552
BryanARM <bryanassis244@gmail.com>
2025-05-08 22:39:57 -0300

    feat(89): Adds clockOutTime to editClockInModal

--- src/components/Modals/EditClockInModal.vue
+++ src/components/Modals/EditClockInModal.vue
@@ -27,6 +27,7 @@ export default {
 
   setup(props, { emit }) {
     const clockInTime = ref('');
+    const clockOutTime = ref('');
     const companyName = ref('');
     const employeeName = ref('');
     const employeeRole = ref('');
@@ -46,9 +47,10 @@ export default {
       loading.value = true;
       const params = {
         id: props.clockIn.key,
-        date_time: clockInTime.value,
+        date_time_in: clockInTime.value,
+        date_time_out: clockOutTime.value,
         employee: props.clockIn.employeeId,
-        direction: props.clockIn.direction
+        direction: props.clockIn.direction,
       };
       try {
         await clockInOut.edit(params);
@@ -65,11 +67,13 @@ export default {
       employeeName.value = props.clockIn.employee;
       companyName.value = props.clockIn.company;
       employeeRole.value = props.clockIn.role;
-      clockInTime.value = props.clockIn.datetime;
+      clockInTime.value = props.clockIn.date_time_in;
+      clockOutTime.value = props.clockIn.date_time_out;
     });
 
     return {
       clockInTime,
+      clockOutTime,
       companyName,
       editClockIn,
       employeeName,
@@ -123,7 +127,14 @@ export default {
       <div class="col-12">
         <at-number-input
           v-model:value="clockInTime"
-          placeholder="Horário do registro"
+          placeholder="Horário de entrada"
+          mask="####-##-## ##:##"
+        />
+      </div>
+      <div class="col-12">
+        <at-number-input
+          v-model:value="clockOutTime"
+          placeholder="Horário de saída"
           mask="####-##-## ##:##"
         />
       </div>
