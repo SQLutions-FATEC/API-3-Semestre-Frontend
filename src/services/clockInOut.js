84de7572089c903dc255916e595ba30fd7c5f9c2
BryanARM <bryanassis244@gmail.com>
2025-05-12 19:10:18 -0300

    refactor(89): Removes temporary code to diagnose errors

--- src/services/clockInOut.js
+++ src/services/clockInOut.js
@@ -3,18 +3,7 @@ import api from './api';
 const clockInOut = {
   get: (params = { page: 1, size: 10 }) =>
     api.get('/clock_in/search', { params }),
-  //remove later
-  edit: async (params) => {
-    console.log('Edit params:', params);
-    try {
-      const response = await api.put(`clock_in/${params.id}`, params);
-      console.log('Edit response:', response);
-      return response;
-    } catch (error) {
-      console.error('Edit error:', error);
-      throw error;
-    }
-  },
+  edit: (params) => api.put(`/clock_in/${params.id}`, params),
 };
 
 export default clockInOut;
