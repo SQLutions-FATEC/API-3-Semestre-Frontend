244173051d6fc534bdb404d03011d72d4d2325b3
GloBrito <gloriafbrito@gmail.com>
2025-05-16 20:22:25 -0300

    fix (79): format date columns in Home.vue table

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,5 +1,6 @@
 <script>
 import HomeHeader from '@/components/Headers/HomeHeader.vue';
+import dayjs from 'dayjs';
 import EditClockInModal from '@/components/Modals/EditClockInModal.vue';
 import clockInOut from '@/services/clockInOut';
 import { eventBus } from '@/utils/eventBus';
@@ -132,15 +133,17 @@ export default {
         },
       },
       {
-        title: 'Data de entrada',
-        dataIndex: 'date_time_in',
-        key: 'datetime',
-      },
-      {
-        title: 'Data de saída',
-        dataIndex: 'date_time_out',
-        key: 'datetime',
-      },
+         title: 'Data de entrada',
+         dataIndex: 'date_time_in',
+         key: 'datetime_in',
+         customRender: ({ text }) => dayjs(text).format('DD/MM/YYYY HH:mm'),
+       },
+       {
+         title: 'Data de saída',
+         dataIndex: 'date_time_out',
+         key: 'datetime_out',
+         customRender: ({ text }) => text ? dayjs(text).format('DD/MM/YYYY HH:mm') : '',
+       },
       {
         title: 'Empresa',
         dataIndex: 'company',
